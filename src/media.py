231ee2186a1fc958b11a7a9782ffab3b9179e744
Lucas Sena <79860759+LucSena@users.noreply.github.com>
2026-05-05 20:57:04 -0400

    green: implementacao da classe Media

--- src/media.py
+++ src/media.py
@@ -1,9 +1,15 @@
+from .exceptions import ListaNotasVaziaError
+
 class Media:
     def media_simples(self, notas: list) -> float:
-        pass
+        if len(notas) == 0:
+            raise ListaNotasVaziaError
+        return sum(notas) / len(notas)
 
     def validar_intervalo(self, nota: float) -> bool:
-        pass
+        return nota >= 0.0 and nota <= 10.0
 
     def verificar_discrepancia(self, notas: list) -> bool:
-        pass
+        if len(notas) == 0:
+            raise ListaNotasVaziaError
+        return max(notas) - min(notas) > 5.0
