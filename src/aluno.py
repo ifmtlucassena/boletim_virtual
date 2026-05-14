63b84c518e149a7fb8bfd1d17c863d9742a0f5a7
AndreyMello <andrey.mello@hotmail.com>
2026-05-14 14:41:52 -0400

    green: implementacao da classe Aluno

--- src/aluno.py
+++ src/aluno.py
@@ -6,16 +6,15 @@ class Aluno:
         self.notas = []
 
     def adicionar_nota(self, nota: float) -> None:
-        if nota < 0 or nota > 10:
+        if not isinstance(nota, (int, float)) or nota < 0 or nota > 10:
             raise NotaInvalidaError
-        self.notas.append(nota)
+        self.notas.append(float(nota))
 
     def get_notas(self) -> list:
         return self.notas
 
     def limpar_notas(self) -> None:
-        pass
+        self.notas = []
 
     def get_quantidade_notas(self) -> int:
-        pass
-
+        return len(self.notas)
